537c6b304e49221c7fac4885900865e31a17c0a0
Dmitrii Goriunov <goriunovd@gmail.com>
2018-05-28 14:43:19 +1200

    close commit added version 2

--- index.js
+++ index.js
@@ -7,13 +7,19 @@ module.exports = function atbash(text, encrypt = false) {
   const toText = encrypt ? CIPHER : ALPHABET;
   const fromText = encrypt ? ALPHABET : CIPHER;
 
+  // version 1
+  // for (let i = 0, leng = text.length; i < leng; i++) {
+  //   const letterIndex = fromText.indexOf(text[i].toLowerCase());
+  //   if (letterIndex === -1) {
+  //     result += text[i];
+  //     continue;
+  //   }
+  //   result += toText[letterIndex];
+  // }
+
+  // version 2
   for (let i = 0, leng = text.length; i < leng; i++) {
-    const letterIndex = fromText.indexOf(text[i].toLowerCase());
-    if (letterIndex === -1) {
-      result += text[i];
-      continue;
-    }
-    result += toText[letterIndex];
+    result += toText[fromText.indexOf(text[i].toLowerCase())] || text[i];
   }
 
   return result;
